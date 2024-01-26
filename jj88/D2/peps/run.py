8f97059a0061c52df3e2d676b5653f0de51fd08e
Ruojing Peng <rppeng@caltech.edu>
2024-01-25 16:33:18 -0800

    push

--- jj88/D2/peps/run.py
+++ jj88/D2/peps/run.py
@@ -3,10 +3,9 @@ from quimb.tensor.tensor_2d_vmc import (
     J1J2,
     AmplitudeFactory2D,
     ExchangeSampler2D,
-    set_options,
 )
 from quimb.tensor.tensor_vmc import (
-    TNVMC,
+    SR,
     scale_wfn,
     write_tn_to_disc,load_tn_from_disc,
 )
@@ -21,21 +20,20 @@ D = 2
 J1 = 1.
 J2 = 0.5
 chi = 8
-set_options(deterministic=False)
 step = 0 
 if step==0:
-    fpeps = load_tn_from_disc(f'../psi50')
+    psi = load_tn_from_disc(f'../psi50')
     config = [(i+j)%2 for i,j in itertools.product(range(Lx),range(Ly))]
     config = np.array([config])
 else:
-    fpeps = load_tn_from_disc(f'psi{step}')
+    psi = load_tn_from_disc(f'psi{step}')
     config = np.load(f'config{step-1}.npy')
 scale = 1.2
 #fpeps = scale_wfn(fpeps,scale)
 #fpeps = load_tn_from_disc(f'./psi100')
 
-af = AmplitudeFactory2D(fpeps,max_bond=chi)
-af.model = J1J2(J1,J2,Lx,Ly)
+model = J1J2(J1,J2,Lx,Ly)
+af = AmplitudeFactory2D(psi,model,max_bond=chi)
 
 burn_in = 40
 sampler = ExchangeSampler2D(Lx,Ly,burn_in=burn_in) 
@@ -43,23 +41,17 @@ sampler = ExchangeSampler2D(Lx,Ly,burn_in=burn_in)
 sampler.af = af
 sampler.config = tuple(config[RANK%config.shape[0],:])
 
-tnvmc = TNVMC(sampler,normalize=True,optimizer='sr',pure_newton=False,solve_full=True,solve_dense=False)
+tnvmc = SR(sampler,normalize=True,solve_full=True,solve_dense=False)
 tnvmc.tmpdir = './' 
 start = step 
-stop = step + 50
+stop = step + 100
 tnvmc.rate1 = .1
-tnvmc.rate2 = .5
 tnvmc.cond1 = 1e-3
-tnvmc.cond2 = 1e-3
 tnvmc.check = 'energy'
 
 tnvmc.batchsize = int(5e4 + .1)
-tnvmc.batchsize_small = int(5e3 + .1)
 if RANK==0:
     print('SIZE=',SIZE)
     print('Lx,Ly=',Lx,Ly)
     print('D=',D)
-    print('rate=',tnvmc.rate1,tnvmc.rate2)
-    print('cond=',tnvmc.cond1,tnvmc.cond2)
-    print('nparams=',af.nparam)
-tnvmc.run(start,stop)
+tnvmc.run(start,stop,save_wfn=True)
