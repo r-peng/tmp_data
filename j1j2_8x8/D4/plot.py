f2b1bf79d478bdb6b83be086dfd02002a353424d
Ruojing Peng <rppeng@login3.cm.cluster>
2024-08-05 10:47:43 -0700

    push

---
+++ j1j2_8x8/D4/plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py 
+from scipy import optimize
+plt.rcParams.update({'font.size':16})
+plt.rcParams.update({'figure.figsize':(6.4,4.8)})
+#plt.rcParams.update({'font.family':'serif'})
+#plt.rcParams.update({'text.usetex':True})
+
+exact = -0.48373
+ref = -0.48296
+fig,ax = plt.subplots(nrows=1,ncols=1)
+
+for tmpdir in ('brick','peps'):
+    out = open(tmpdir+'/summary.out', 'r').readlines()
+    e = []
+    for l in out:
+        if l[:len('step=')]=="step=":
+            ls = l.split(',')
+            e.append(float(ls[1].split('=')[-1]))
+    e = np.array(e)
+    e = np.log10(np.fabs((e-exact)/exact)) 
+    ax.plot(range(len(e)),e,linestyle='-',label=tmpdir)
+
+e = np.log10(np.fabs((ref - exact)/exact))
+ax.plot(range(100),np.ones(100)*e,linestyle='-',color='k')
+    
+ax.set_xlabel('step')
+ax.set_ylabel('log10(rel err)')
+ax.legend()
+plt.subplots_adjust(left=0.2, bottom=0.15, right=0.95, top=0.99)
+fig.savefig(f"j1j2_8x8_D4.png", dpi=250)
